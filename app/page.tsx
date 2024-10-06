c244ae94b11c039aef845c875055281b5c85eba9
Martin Lowe <martinlowe673@gmail.com>
2024-10-06 13:10:14 +0100

    looking okay

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 const World = dynamic(() => import("../components/ui/globe").then((m) => m.World), {
   ssr: false,
@@ -395,7 +398,18 @@ export default function Page() {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center py-20 h-screen  dark:bg-black bg-white relative w-full">
+    <div className="flex flex-col items-center justify-center py-20 h-screen  dark:bg-black bg-white relative w-full">
+      <div className=" flex flex-col justify-center items-center gap-3">
+      <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
+         AI Travel Planner
+          </h2>
+          <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
+            With this planner you can plan any trip in the world
+          </p>
+          <Link href={"/travel-planner"}>
+        <Button>Plan your next trip</Button>
+        </Link>
+      </div>
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
         <motion.div
           initial={{
@@ -409,14 +423,10 @@ export default function Page() {
           transition={{
             duration: 1,
           }}
-          className="div"
+          className=""
         >
-          <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
-         AI Travel Planner
-          </h2>
-          <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
-            With this planner you can plan any trip in the world
-          </p>
+         
+        
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
         <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
